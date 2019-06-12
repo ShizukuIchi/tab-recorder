9b6f812c7f5b3729b9e1061bcc44db3e782bbf21
ShizukuIchi <shizukuichi@gmail.com>
2019-06-12 12:58:22 +0800

    easy

--- event.js
+++ event.js
@@ -1,7 +1,6 @@
 chrome.browserAction.onClicked.addListener(function() {
-  chrome.tabs.executeScript(null, {file: "content_script.js"});
   chrome.tabs.create({
-      url: "preview.html",
-      active: false
+    url: 'preview.html',
+    active: false,
   });
 });
--- js/main.js
+++ js/main.js
@@ -3,27 +3,22 @@
 let mediaRecorder;
 let recordedBlobs = [];
 let recordOptions = { mimeType: 'video/webm;codecs=vp9' };
-let cursor = {x:0, y:0};
-let cursorBeforePause = {x:0, y:0};
-let blob;
-let timer;
- 
 
 const recordedVideo = document.querySelector('video#recorded');
 
+window.onfocus = blob2Src;
+
 function blob2Src() {
-  const superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
-  recordedVideo.src = window.URL.createObjectURL(superBuffer); 
-  window.onfocus = function() {
-    recordedVideo.controls = true;
-    recordedVideo.play()
-  }
-};
+  if (recordedVideo.src) return;
+  mediaRecorder.stop();
+  const superBuffer = new Blob(recordedBlobs, { type: 'video/webm' });
+  recordedVideo.src = window.URL.createObjectURL(superBuffer);
+  recordedVideo.play();
+}
 
 function handleDataAvailable(event) {
   if (event.data && event.data.size > 0) {
-    blob = event.data
-    recordedBlobs.push(blob);
+    recordedBlobs.push(event.data);
   }
 }
 
@@ -34,50 +29,21 @@ function handleSuccess(stream) {
     console.error(`Exception while creating MediaRecorder: ${e}`);
     return;
   }
-  timer = setTimeout(()=> mediaRecorder.pause(), 100);
   mediaRecorder.ondataavailable = handleDataAvailable;
-  mediaRecorder.onpause = function(){ 
-    window.port.postMessage({ msg: 'pause' }) 
-  }
-  mediaRecorder.onresume = function(){ window.port.postMessage({ msg: 'resume' }) }
   mediaRecorder.start(10);
 }
 
 chrome.tabCapture.capture(
   {
-    video: true, 
+    video: true,
     audio: true,
     videoConstraints: {
       mandatory: {
         maxWidth: window.innerWidth,
         maxHeight: window.innerHeight,
-        maxFrameRate: 60
+        maxFrameRate: 60,
       },
     },
-  }, handleSuccess
+  },
+  handleSuccess,
 );
-
-chrome.runtime.onConnect.addListener(function(port) {
-  console.assert(port.name == "page");
-  window.port = port
-  port.onMessage.addListener(function(msg) {
-    if(msg.type == 'mousemove'){
-      cursor.x = msg.x;
-      cursor.y = msg.y;
-    } else if(mediaRecorder.state){
-      if (mediaRecorder.state == 'paused') {
-        mediaRecorder.resume()
-        port.postMessage({msg:`(${cursorBeforePause.x},${cursorBeforePause.y})->(${cursor.x},${cursor.y})`})
-      }
-      clearTimeout(timer)
-      timer = setTimeout(()=>{
-        blob2Src()
-        mediaRecorder.pause()
-        cursorBeforePause.x = cursor.x
-        cursorBeforePause.y = cursor.y
-      }, 100)
-    }
-  });
-});
-
- 
